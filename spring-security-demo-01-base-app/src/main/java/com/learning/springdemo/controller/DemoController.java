e3986d3666052cfcaae53fdd691bb97a54ab8fcf
Sritallas <srilakshmi.tallas@gmail.com>
2022-10-04 14:22:06 -0400

    Pushing into Spring security branch

---
+++ spring-security-demo-01-base-app/src/main/java/com/learning/springdemo/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.learning.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+	@GetMapping("/")
+	public String homePage() {
+		
+		return "homePage";
+	}
+}
